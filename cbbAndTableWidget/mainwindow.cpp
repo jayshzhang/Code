5baf7010cbcbffa4d3d2ec95cae0dbb75d0b65c1
jayshzhang <jayshzhang@hotmail.com>
2017-06-09 00:59:01 +0800

    cellClicked

--- cbbAndTableWidget/mainwindow.cpp
+++ cbbAndTableWidget/mainwindow.cpp
@@ -2,19 +2,23 @@
 #include "ui_mainwindow.h"
 #include <QVBoxLayout>
 #include <QComboBox>
-
+#include <QSignalMapper>
+#include <QDebug>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     QStringList header;
     header << "first" << "second" << "third";
     ui->tableWidget->setColumnCount(3);
-    ui->tableWidget->setRowCount(3);
+    ui->tableWidget->setRowCount(4);
     ui->tableWidget->setHorizontalHeaderLabels(header);
     ui->tableWidget->horizontalHeader()->setDefaultAlignment(Qt::AlignLeft|Qt::AlignVCenter);
     ui->tableWidget->horizontalHeader()->setVisible(true);
+
+    //放进去多个combobox
     for(int i = 0; i < 3; i++ )
     {
         QStringList newStr;
@@ -23,8 +27,18 @@ MainWindow::MainWindow(QWidget *parent) :
         initCbb->addItems(newStr);
         ui->tableWidget->setCellWidget(i,1, initCbb);
     }
+    //信号和槽的连接
+    connect(ui->tableWidget,&QTableWidget::cellClicked,this,
+            &MainWindow::doSth);
+
 }
 
+void MainWindow::doSth()
+{
+    //企图在这里得到行号
+    int rowNum = ui->tableWidget->currentRow();
+    qDebug() << "current row is :" << QString::number(rowNum, 10);
+}
 MainWindow::~MainWindow()
 {
     delete ui;
